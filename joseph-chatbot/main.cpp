bf98ce66720f3e345982fcf33a8094b70cde381e
imann24 <imann24@gmail.com>
2015-10-01 13:54:05 -0400

    Initial Commit

---
+++ joseph-chatbot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  joseph-chatbot
+//
+//  Created by imann24 on 10/1/15.
+//  Copyright © 2015 imann24. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
