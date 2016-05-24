5cffc6066770a125e16461758f0607e167b9327d
imann24 <contact@isaiahmann.com>
2016-05-24 12:06:22 -0400

    Initial commit

--- joseph-chatbot/main.cpp
+++ joseph-chatbot/main.cpp
@@ -7,9 +7,56 @@
 //
 
 #include <iostream>
+using namespace std;
+
+enum Mood {HAPPY, SAD, NEUTRAL, ANGRY};
+
+
+string getPunctuation (Mood currentMood) {
+    if (currentMood == HAPPY) {
+        return "!";
+    } else if (currentMood == SAD) {
+        return "...";
+    } else if (currentMood == ANGRY) {
+        return "!!!";
+    } else if (currentMood == NEUTRAL) {
+        return ".";
+    } else {
+        return ".";
+    }
+}
+
+
+
+bool sayingGoodbye (string input) {
+    return input == "bye";
+}
+
 
 int main(int argc, const char * argv[]) {
+    string name;
+    
+    Mood currentMood = ANGRY;
+    
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, I'm Joseph, what's your name?\n";
+    
+    // Find out how to read in a multiword strings C++
+    cin >> name;
+    
+    cout << name + " is a great name" + getPunctuation(currentMood) + " Tell me a little more about yourself"<< endl;
+
+    string input = "";
+    
+    while (!sayingGoodbye(input)) {
+        cin >> input;
+        
+        if (sayingGoodbye(input)) {
+            cout << "It's been good talking. See you" << endl;
+        } else {
+            cout << input + " sounds so interesting. Tell me more or type 'bye' to exit" << endl;
+        }
+    }
+    
     return 0;
 }
